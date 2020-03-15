bbd7ef9a0a4c571385614a0e18b860ce2925b0db
caiyihang <517511131@qq.com>
2020-03-15 19:58:52 +0800

    start text

--- gmall-text/src/main/java/com/atguigu/gmall/text.java
+++ gmall-text/src/main/java/com/atguigu/gmall/text.java
@@ -2,8 +2,6 @@ package com.atguigu.gmall;
 
 public class text {
     public static void main(String[] args) {
-        System.out.println("day15");
-        System.out.println("day15-2");
-        System.out.println("text");
+        System.out.println("git text over");
     }
 }
