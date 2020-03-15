4b9fac7d2e5b7e5a5431b73f42ea577dcf9d1440
caiyihang <517511131@qq.com>
2020-03-15 20:02:10 +0800

    over git text

--- gmall-text/src/main/java/com/atguigu/gmall/text.java
+++ gmall-text/src/main/java/com/atguigu/gmall/text.java
@@ -3,5 +3,6 @@ package com.atguigu.gmall;
 public class text {
     public static void main(String[] args) {
         System.out.println("git text over");
+        System.out.println("day16");
     }
 }
